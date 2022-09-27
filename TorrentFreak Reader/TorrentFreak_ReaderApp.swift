0e114ab9563a066cf76db958b5c892fe699845b2
Parker Madel <pmadel12@gmail.com>
2022-09-27 18:20:42 -0500

    Initial Commit

---
+++ TorrentFreak Reader/TorrentFreak_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TorrentFreak_ReaderApp.swift
+//  TorrentFreak Reader
+//
+//  Created by Parker Madel on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct TorrentFreak_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TorrentFreak ReaderUITests/TorrentFreak_ReaderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TorrentFreak_ReaderUITestsLaunchTests.swift
+//  TorrentFreak ReaderUITests
+//
+//  Created by Parker Madel on 9/27/22.
+//
+
+import XCTest
+
+final class TorrentFreak_ReaderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
