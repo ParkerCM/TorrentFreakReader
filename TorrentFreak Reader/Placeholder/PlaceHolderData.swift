9162d780c4b3e714e0dbf37eb13631c4f704f21c
Parker Madel <pmadel12@gmail.com>
2022-10-03 19:17:01 -0500

    Improved placeholder data

---
+++ TorrentFreak Reader/Placeholder/PlaceHolderData.swift
@@ -0,0 +1,68 @@
+//
+//  PlaceHolderData.swift
+//  TorrentFreak Reader
+//
+//  Created by Parker Madel on 10/3/22.
+//
+
+import Foundation
+
+class PlaceHolderData {
+    
+    public static let articles = [
+        Article(title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pharetra pharetra massa massa ultricies.",
+                author: "Andy", imageUrl: "https://torrentfreak.com/images/russia-kremlin.jpg",
+                articleUrl: "null",
+                category: "Lawsuits",
+                date: "today",
+                isLeading: true),
+        Article(title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Interdum posuere lorem ipsum dolor sit.",
+                author: "Andy",
+                imageUrl: "https://torrentfreak.com/images/russia-kremlin.jpg",
+                articleUrl: "null",
+                category: "Stats",
+                date: "today",
+                isLeading: false),
+        Article(title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas integer eget.",
+                author: "Andy",
+                imageUrl: "https://torrentfreak.com/images/russia-kremlin.jpg",
+                articleUrl: "null",
+                category: "Anti-Piracy",
+                date: "today",
+                isLeading: false),
+        Article(title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas sed enim ut sem viverra aliquet eget.",
+                author: "Andy",
+                imageUrl: "https://torrentfreak.com/images/russia-kremlin.jpg",
+                articleUrl: "null",
+                category: "Opinion Article",
+                date: "today",
+                isLeading: false),
+        Article(title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna condimentum mattis pellentesque id.",
+                author: "Andy",
+                imageUrl: "https://torrentfreak.com/images/russia-kremlin.jpg",
+                articleUrl: "null",
+                category: "Lawsuits",
+                date: "today",
+                isLeading: false),
+        Article(title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non enim praesent elementum facilisis.",
+                author: "Andy",
+                imageUrl: "https://torrentfreak.com/images/russia-kremlin.jpg",
+                articleUrl: "null",
+                category: "Lawsuits",
+                date: "today",
+                isLeading: false),
+        Article(title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rutrum quisque non tellus orci.",
+                author: "Andy",
+                imageUrl: "https://torrentfreak.com/images/russia-kremlin.jpg",
+                articleUrl: "null",
+                category: "Lawsuits",
+                date: "today",
+                isLeading: false),
+        Article(title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec massa sapien faucibus et molestie ac feugiat.",
+                author: "Andy",
+                imageUrl: "https://torrentfreak.com/images/russia-kremlin.jpg",
+                articleUrl: "null",
+                category: "Lawsuits",
+                date: "today",
+                isLeading: false)]
+}
