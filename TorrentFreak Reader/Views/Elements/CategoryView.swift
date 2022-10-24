c5a2adb828731c62466fff22825c1fc855261af9
Parker Madel <pmadel12@gmail.com>
2022-10-24 13:42:45 -0500

    More category colors but the implementation still sucks

--- TorrentFreak Reader/Views/Elements/CategoryView.swift
+++ TorrentFreak Reader/Views/Elements/CategoryView.swift
@@ -39,6 +39,32 @@ struct CategoryView: View {
             return .green
         case "Opinion Article":
             return .cyan
+        case "IPTV and Streaming":
+            return.brown
+        case "DMCA":
+            return .pink
+        case "DVDrip":
+            return .mint
+        case "Repeat Infringer":
+            return .indigo
+        case "Apps and Sites":
+            return .teal
+        case "Site Blocking":
+            return .gray
+        case "Copyright Trolls":
+            return .red
+        case "Takedowns and Seizures":
+            return .red
+        case "Piracy Research":
+            return .red
+        case "Technology":
+            return .blue
+        case "VPN Providers":
+            return .orange
+        case "Bittorrent":
+            return .indigo
+        case "Digital Freedon":
+            return .green
         default:
             return .cyan
         }
